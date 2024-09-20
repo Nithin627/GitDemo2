c3a8ed348aa625d28f71e7eb020ce39de77f47d6
Nithin <nithinpaddyur@gmail.com>
2024-09-20 22:38:29 +0530

    first Commit

---
+++ src/test/java/com/nithin/poi/Datadriven.java
@@ -0,0 +1,57 @@
+package com.nithin.poi;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class Datadriven {
+
+	@Test(dataProvider = "getData")
+	public void loginTest(String name, String password) {
+
+	}
+
+	public Object[][] getData() throws IOException {
+//		converts data into string
+		DataFormatter formatter = new DataFormatter();
+
+//		create file input stream
+		String path = "";
+		FileInputStream fis = new FileInputStream(path);
+
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
+
+//	    get the row count
+		int rowCount = sheet.getPhysicalNumberOfRows();
+
+//	   get cloumns
+		XSSFRow row = sheet.getRow(0);
+		int colCount = row.getLastCellNum();
+
+		Object[][] data = new Object[rowCount - 1][colCount];
+
+		for (int i = 0; i < rowCount - 1; i++) {
+			row = sheet.getRow(i + 1);
+			for (int j = 0; j < colCount; j++) {
+
+				XSSFCell cell = row.getCell(j);
+
+				data[i][j] = formatter.formatCellValue(cell);
+
+			}
+		}
+
+//	    
+		return data;
+//		create the workbook 
+
+	}
+
+}
